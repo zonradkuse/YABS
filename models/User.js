8f2c3859641fe66c1ed7029f3b985048f15d172d
Johannes Neuhaus <jneuhaus94@gmail.com>
2014-11-22 21:51:41 +0100

    added small user model idea - will be extended soon

---
+++ models/User.js
@@ -0,0 +1,9 @@
+/*! The User Model
+* @param {String} pName The useres name as it is visible for the system.
+* @param {String[]} pAccess This holds the access rights.
+*/
+
+exports = function(pName, pAccess){
+  this.name = pName;
+  this.sessionId = pSessId; 
+}
