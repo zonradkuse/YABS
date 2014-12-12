9251d64d0f567ef31aba222a9912dedfa0b8dc23
Johannes Neuhaus <johannes.neuhaus@rwth-aachen.de>
2014-12-12 18:26:45 +0100

    added caching

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ app.use(session({
    saveUninitialized: true
 }));
 
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/public', { maxAge: 86400000}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
