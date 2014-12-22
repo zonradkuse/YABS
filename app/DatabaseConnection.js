9d3e61b2459c8963f9567253b5a134e7c97ceb86
Johannes Neuhaus <johannes.neuhaus@rwth-aachen.de>
2014-12-22 20:02:11 +0100

    begin with redis implementation

--- app/DatabaseConnection.js
+++ app/DatabaseConnection.js
@@ -1,10 +1,59 @@
 var redis = require('redis');
-
 exports.addNewQuestion = addNewQuestion;
 //todo more functions
 
-function addNewQuestion(threadID){
+function initClient(){
+    var client = redis.createClient();
+    client.on('error', function(err){
+        //do some fancy error handling
+    });
+    return client;
+}
+function addNewThread(thread){
+    var client = initclient();
+    client.hset('system:thread', 'id', thread.id);
+    client.close();
+}
+
+function addNewQuestion(thread, question){
+    var client = initClient();
+    client.hmset('system:question', 'threadId', thread.id, 'qId', question.qid, 'qContent', q.content);
+    client.close();
+}
+
+function addNewUser(user){
 
 }
 
+function addNewRoom(room){
+
+}
+
+function addNewAnswer(question, answer){
+
+}
+
+function getThreadById(id){
+
+}
+
+function getQuestionById(id){
+
+}
+
+function getUserById(id){
+
+}
+
+function getUserByName(name){
+
+}
+
+function getRoomById(id){
+
+}
+
+function getRoomByName(name){
+
+}
 /* TODO: Redis abstraction */
