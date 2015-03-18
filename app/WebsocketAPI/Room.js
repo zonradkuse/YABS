e943a37d3786a61bce6f63f28628d8114c816680
Jens Piekenbrinck <magic1995221@web.de>
2015-03-18 16:47:54 +0100

    room:getQuestions

--- app/WebsocketAPI/Room.js
+++ app/WebsocketAPI/Room.js
@@ -1,6 +1,27 @@
+var roomDAO = require('../../models/Room.js');
+var userDAO = require('../../models/User.js');
+
+
 module.exports = function(wsControl){
-    wsControl.on("room:getQuestion", function(wss, ws, session, params, interfaceEntry, refId, sId){
+    wsControl.on("room:getQuestions", function(wss, ws, session, params, interfaceEntry, refId, sId){
         //params.roomId
+        if(session.user && params.roomId){
+			userDAO.getRoomAccess(session.user, {population: 'questions.author access.questions.answers.author'}, function(err, rooms){
+            	for(var i=0; i<rooms.length; i++){
+            		if(rooms[i]._id == params.roomId){
+            			wsControl.build(ws, null, {
+		                    'questions': rooms[i].questions,
+		                }, refId);
+            			return;
+            		}
+            	}
+            	wsControl.build(ws, new Error("Access denied."), null, refId);
+            });
+        }
+        else{
+        	wsControl.build(ws, new Error("Your session is invalid."), null, refId);
+        }
+
     });
 
     wsControl.on("room:getAnswers", function(wss, ws, session, params, interfaceEntry, refId, sId){
