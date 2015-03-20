8e15a137bab5ac9761e6702cde4993ba72456bb4
Johannes Neuhaus <johannes.neuhaus@rwth-aachen.de>
2015-03-21 00:43:03 +0100

    D A

--- test/benchmark.js
+++ test/benchmark.js
@@ -1,6 +1,9 @@
 (function(){
     var WebSocket = require("ws");
     var Benchmark = function(number){
+
+        //var ws = new WebSocket("ws://52.28.3.175:8080"); //amazon
+	    //var ws = new WebSocket("ws://192.168.0.105:8080"); //local
         var ws = new WebSocket("ws://lanzarote.informatik.rwth-aachen.de:8080");
         var start;
         ws.on("message", function(event){
@@ -18,15 +21,17 @@
         });
 
         ws.on("error", function(err){
-            console.log("CONNECTION DUMP");
+            console.log(err);
         });
     }
-
+    
+    var count = 0;
     var j = 1;
     while(j > 0){
         setTimeout(function(){
             for (var i = 1000; i >= 0; i--) {
-                new Benchmark(i);
+                new Benchmark(count);
+		        count++;
             };
         }, j);
         j--;
