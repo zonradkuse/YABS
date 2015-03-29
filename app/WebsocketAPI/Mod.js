4bfafca9b190b2c5f793c4370db85f07fe3cb24b
Johannes Neuhaus <johannes.neuhaus@rwth-aachen.de>
2015-03-29 23:40:37 +0200

    implemented mod: calls and fixed connection opening bug

--- app/WebsocketAPI/Mod.js
+++ app/WebsocketAPI/Mod.js
@@ -1,7 +1,74 @@
 var answerDAO = require('../../models/Answer.js');
+var questionDAO = require('../../models/Question.js');
 var accessManager = require('../AccessManagement.js');
 
 module.exports = function(wsControl) {
+    wsControl.on("mod:deleteAnswer", function(wss, ws, session, params, interfaceEntry, refId, sId, authed){
+        if(authed) {
+            if(params.roomId && params.questionId && params.answerId) {
+                accessManager.checkAccessBySId("mod:markAsAnswer", sId, params.roomId, function(err, bool){
+                    if (bool) {
+                        answerDAO.getByID(params.answerId, {population: 'author'}, function(err, ans){
+                            ans.images = [];
+                            ans.content = "Der Inhalt wurde gelöscht.";
+                            q.save(function(err){
+                                if (err){
+                                    wsControl.build(ws, new Error("Could not save the new state."), null, refId);
+                                    return logger.err("Could not save the new state: " + err);
+                                }
+                                ans.author = ans.author.local;
+                                wss.roomBroadcast(ws, "answer:add", {
+                                    'roomId': params.roomId,
+                                    'questionId' : params.questionId,
+                                    'answer' : ans
+                                }, params.roomId);
+                            });
+                        });
+                    } else {
+                        wsControl.build(ws, new Error("Access Denied."), null, refId);
+                    }
+                });
+            } else {
+                wsControl.build(ws, new Error("Missing Parameters."), null, refId);
+            }
+        } else {
+            wsControl.build(ws, new Error("Access Denied."), null, refId);
+        }
+    });
+
+    wsControl.on("mod:deleteQuestion", function(wss, ws, session, params, interfaceEntry, refId, sId, authed){
+        if(authed) {
+            if(params.roomId && params.questionId) {
+                accessManager.checkAccessBySId("mod:markAsAnswer", sId, params.roomId, function(err, bool){
+                    if (bool) {
+                        questionDAO.getByID(params.questionId, {population: 'author'}, function(err, q){
+                            q.images = [];
+                            q.content = "Der Inhalt wurde gelöscht.";
+                            q.votes = [];
+                            q.answers = [];
+                            q.save(function(err){
+                                if (err){
+                                    wsControl.build(ws, new Error("Could not save the new state."), null, refId);
+                                    return logger.err("Could not save the new state: " + err);
+                                }
+                                ans.author = ans.author.local;
+                                wss.roomBroadcast(ws, "question:add", {
+                                    'roomId': params.roomId,
+                                    'question' : q
+                                }, params.roomId);
+                            });
+                        });
+                    } else {
+                        wsControl.build(ws, new Error("Access Denied."), null, refId);
+                    }
+                });
+            } else {
+                wsControl.build(ws, new Error("Missing Parameters."), null, refId);
+            }
+        } else {
+            wsControl.build(ws, new Error("Access Denied."), null, refId);
+        }
+    });
     wsControl.on("mod:markAsAnswer", function(wss, ws, session, params, interfaceEntry, refId, sId, authed){
         if (authed) {
             if (params.roomId && params.questionId && params.answerId) {
@@ -11,7 +78,7 @@ module.exports = function(wsControl) {
                             ans.isAnswer = true;
                             ans.save(function(err){
                                 if (err){
-                                    wsControl.build("Could not save the new state.");
+                                    wsControl.build(ws, new Error("Could not save the new state."), null, refId);
                                     return logger.err("Could not save the new state: " + err);
                                 }
                                 ans.author = ans.author.local;
