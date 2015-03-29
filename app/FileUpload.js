d24ec079254649b56c0331070bf24861ccdb816b
Johannes Neuhaus <johannes.neuhaus@rwth-aachen.de>
2015-03-29 20:54:22 +0200

    explosion fix

--- app/FileUpload.js
+++ app/FileUpload.js
@@ -20,8 +20,7 @@ module.exports = function(app) {
         limits: config.multer.options,
         onFileUploadStart : function(file, req, res) {
             // set header
-            console.log(file);
-            console.log(req.files);
+
             res.setHeader('Content-Type', 'application/json');
             // do first checks on file
             if (file === undefined || file === {}) {
@@ -41,13 +40,13 @@ module.exports = function(app) {
                     return false;
                 }
             }
-            console.log("lol")
+            
         },
         onFileSizeLimit: function (file) {
           fs.unlink('../public/' + file.path); // delete the partially written file
         },
         onFileUploadComplete: function(file, req, res) {
-            console.log(file);
+            
             if (file.size >= config.multer.options.fileSize) {
                 res.write(JSON.stringify({error : "Filesize limit exceeded."}));
                 req.files = undefined; //indicator to upload route
