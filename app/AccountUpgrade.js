d11f42b003097499831396e4b5914156633ab475
Johannes Neuhaus <johannes.neuhaus@rwth-aachen.de>
2015-04-10 03:40:03 +0200

    updated logic to use passwords

--- app/AccountUpgrade.js
+++ app/AccountUpgrade.js
@@ -1,25 +1,8 @@
-var nodemailer = require('nodemailer');
-var addresses = require('../config/mails.json').data;
+var addresses = require('../config/passwords.json').data;
 var config = require('../config.json');
 var logger = require('./Logger.js');
 var roomDAO = require('../models/Room.js');
-var adminkey = "wurstbrot";
 var roles = require('../config/UserRoles.json');
-
-var transporter = nodemailer.createTransport({
-    service: 'Gmail',
-    auth: {
-        user: 'noreply.yabs@gmail.com',
-        pass: 'a1b2c3d4$'
-    }
-});
-
-var mailOptions = {
-    from: 'YABS <noreply.yabs@gmail.com>',
-    to: '',
-    subject: 'YABS admin access',
-    text: '',
-};
 
 
 module.exports = function(app){
@@ -35,71 +18,26 @@ module.exports = function(app){
         if (req.session && req.session.user) {
             res.sendFile(path.resolve(__dirname, '../', 'public/upgrade.html'));
         } else {
-            res.write('you are not logged in.');
-            res.end();
+            res.sendStatus(403);
         }
     });
 
     app.post('/roles/admin/:roomId', function(req, res){
-        if(req.params.roomId && req.body.email) {
+        if(req.params.roomId && req.body.password) {
             roomDAO.getByID(req.params.roomId, {population : ''}, function(err, room){
-                if(err) res.write(err.message);
+                if(err) res.json(err.message);
                 if(room) {
-                    //room exists send mail if adress is in list or is a cs.rwth-aachen.de adress
-                    if(addresses.indexOf(req.body.email.toLowerCase()) > -1 || req.body.email.toLowerCase().indexOf('@cs.rwth-aachen.de') > -1) {
-                        var hash = require('crypto').createHash('sha1').update(req.params.roomId + adminkey + req.session.user._id).digest('hex');
-                        mailOptions.text = "Please visit: " + req.protocol + "://" + req.get('Host') + '/roles/admin/' + req.params.roomId + '/' + hash;
-                        mailOptions.to = req.body.email;
-                        transporter.sendMail(mailOptions, function(error, info){
-                        if(error){
-                            res.write("An error occured! This has been reported.");
-                            logger.err(error);
-                        }else{
-                            res.write('Message sent! You can now close this window.');
-                        }
-                        res.end();
-                        });
-                    } else {
-                        res.send("Your address is not listed or invalid formatted. Please contact johannes.neuhaus [at] rwth-aachen.de");
-                        res.end();
-                    }
-                } else {
-                    res.write(err.message);
-                    res.end();
-                }
-            });
-        } else {
-            res.write("missing parameters");
-            res.end();
-        }
-    });
-    
-    app.get('/roles/admin/:roomId/:key', function(req, res){
-        if(req.params.roomId && req.params.key && req.session.user) {
-            roomDAO.getByID(req.params.roomId, {population : ''}, function(err, room){
-                if (err) {
-                    res.write(err.message);
-                } else {
-                    
-                    if(req.params.key == require('crypto').createHash('sha1').update(req.params.roomId + adminkey + req.session.user._id).digest('hex')){
-                        //set access right
-                        if(req.session.rights) {
-                            req.session.user.rights.push({roomId : req.params.roomId, accessLevel: roles.defaultAdmin});
-                        } else {
-                            req.session.user.rights.push({roomId : req.params.roomId, accessLevel: roles.defaultAdmin});
-                        }
+                    if(addresses.indexOf(req.body.password) > -1) {
+                        req.session.user.rights.push({roomId : req.params.roomId, accessLevel: roles.defaultAdmin});
                         res.redirect("/course/" + req.params.roomId);
                     } else {
-                        res.write("bad key");
-                        res.end();
+                        res.status(403).send("Wrong Password");
                     }
                 }
             });
         } else {
-            res.write("Missing Field or bad user. Use the browser you used to send the Request.");
-            res.end();
+            res.status(403).send("missing parameters");
         }
-        
     });
     
 };
