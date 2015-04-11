9cae6146301be057d6d32489763189e7c33c6b0c
Johannes Neuhaus <johannes.neuhaus@rwth-aachen.de>
2015-04-11 20:44:16 +0200

    some changes for automatic chart update

--- client/js/controllers/courseController.js
+++ client/js/controllers/courseController.js
@@ -7,16 +7,19 @@ clientControllers.controller("courseController", ["$scope", "$routeParams", "roo
             $scope.chartist.options = {
                 lineSmooth: false, // disable interpolation
                 axisX: {
-                    showLabel: false // disable x label as data grows
+                    showLabel: false, // disable x label as data grows and chart becomes ugly
                 },
                 axisY: {
                     labelInterpolationFnc: function(val) { // do not show half panics
                         return (val % 1 === 0 ? val : ' ');
                     }
-                }
+                } 
             };
 
-            $scope.chartist.lineData = {labels: [], series: [[]]};
+            $scope.chartist.lineData = { labels: ["Monday", "Tuesday", "Wednesday"], series: [
+                [1,2,3],
+                [1,2,7]
+            ]};
             $scope.orderProp = '-votes';
 
         $scope.$watch(function() { return rooms.getById($routeParams.courseid); }, function(room) {
@@ -55,50 +58,12 @@ clientControllers.controller("courseController", ["$scope", "$routeParams", "roo
                     if(level > 1) {
                         $scope.showAdmin = true;
                         $("#statModal").off().on("shown.bs.modal", function() {
-                            rooms.getPanicGraph($scope.room).then(function(data) {
-                                var labels = [];
-                                var values = [];
-                                for (var i = 0; i < data.graph.length; i++) {
-                                    var date = (new Date(data.graph[i].time)).toLocaleTimeString();
-                                    labels.push(date);
-                                    values.push({ value: data.graph[i].panics, meta: date });
-                                }
-                                $scope.chartist.lineData = { labels: labels, series: [{
-                                        name: "Panics",
-                                        data: values }
-                                ]};
-
-                                // modified chartist example
-                                var $chart = $('.ct-chart');
-
-                                var $toolTip = $chart
-                                    .find('.tooltip');
-
-                                $('.ct-chart').on('mouseenter', '.ct-point', function() {
-                                    var $point = $(this);
-                                    var count = $point.attr('ct:value');
-                                    var time = $point.attr('ct:meta');
-                                    // this is bad but scope variables are too slow. gotta go for an angular module
-                                    $toolTip.html('<div class="panel-heading">' + time + 
-                                        '</div><div class="panel-body">' + count + ' Panics</div>').show();
-                                });
-
-                                $chart.on('mouseleave', '.ct-point', function() {
-                                    $toolTip.hide();
-                                });
-
-                                $chart.on('mousemove', function(event) {
-                                    $toolTip.css({
-                                        left: (event.offsetX || event.originalEvent.layerX) - $toolTip.width() / 2 + 14,
-                                        top: (event.offsetY || event.originalEvent.layerY) - $toolTip.height()
-                                    });
-                                });
-
-                                $timeout(function() {
-                                    window.dispatchEvent(new Event("resize"));
-                                    // Rerenders chartist (crappy solution, but the directive doesnt allow direct access)
-                                    return true;
-                                });                                
+                            $scope.drawChart();
+                            var timer = setInterval(function(){
+                                $scope.drawChart(); // redraw every 60 seconds
+                            }, 60 * 1000); 
+                            $('#statModal').on('hide.bs.modal', function(){
+                                clearInterval(timer);
                             });
                         });
                     }
@@ -203,5 +168,53 @@ clientControllers.controller("courseController", ["$scope", "$routeParams", "roo
         $scope.deleteAnswer = function(question, answer) {
             rooms.deleteAnswer($scope.room, question, answer);
         };        
+
+        $scope.drawChart = function() {
+            rooms.getPanicGraph($scope.room).then(function(data) {
+                var labels = [];
+                var values = [];
+                for (var i = 0; i < data.graph.length; i++) {
+                    var date = (new Date(data.graph[i].time)).toLocaleTimeString();
+                    labels.push(date);
+                    values.push({ value: data.graph[i].panics, meta: date });
+                }
+                $scope.chartist.lineData = { labels: labels, series: [{
+                        name: "Panics",
+                        data: values }
+                ]};
+
+                // modified chartist example
+                var $chart = $('.ct-chart');
+
+                var $toolTip = $chart
+                    .find('.tooltip');
+
+                $('.ct-chart').on('mouseenter', '.ct-point', function() {
+                    var $point = $(this);
+                    var count = $point.attr('ct:value');
+                    var time = $point.attr('ct:meta');
+                    // this is bad but scope variables are too slow. gotta go for an angular module
+                    $toolTip.html('<div class="panel-heading">' + time + 
+                        '</div><div class="panel-body">' + count + ' Panics</div>').show();
+                });
+
+                $chart.on('mouseleave', '.ct-point', function() {
+                    $toolTip.hide();
+                });
+
+                $chart.on('mousemove', function(event) {
+                    $toolTip.css({
+                        left: (event.offsetX || event.originalEvent.layerX) - $toolTip.width() / 2 + 14,
+                        top: (event.offsetY || event.originalEvent.layerY) - $toolTip.height()
+                    });
+                });
+
+                $timeout(function() {
+                    window.dispatchEvent(new Event("resize"));
+                    // Rerenders chartist (crappy solution, but the directive doesnt allow direct access)
+                    return true;
+                });                                
+            });
+        };
     }
 ]);
