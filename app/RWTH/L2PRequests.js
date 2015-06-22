b074bfd48456fa78a04e7f81211738c168fcae26
Johannes Neuhaus <johannes.neuhaus@rwth-aachen.de>
2015-06-22 16:08:22 +0200

    refactoring and comments

--- app/RWTH/L2PRequests.js
+++ app/RWTH/L2PRequests.js
@@ -1,16 +1,43 @@
+/**
+ * L2PRequests.js - Collection of L2P API calls.
+ *
+ * TODO refactor this such that every request has a prototype and makeCopy is not needed.
+ * 		Do not touch this before not having refactored the request error todo below.
+ *
+ * Consult L2P API - Sometimes they rename things like status to Status.
+ * https://www3.elearning.rwth-aachen.de/_vti_bin/l2pservices/api.svc/v1/documentation
+ */
+
 var https = require('https');
 var logger = require('../Logger.js');
 
+var options = {
+	host: 'www3.elearning.rwth-aachen.de',
+	port: '443',
+	path: '',
+	method: 'GET',
+	headers: {
+		'Content-Type': 'application/x-www-form-urlencoded',
+	}
+};
+
+function makeCopy() {
+	return JSON.parse(JSON.stringify(options));
+}
+
 function getAllCourses(token, next) {
-	var options = {
-		host: 'www3.elearning.rwth-aachen.de',
-		port: '443',
-		path: '/_vti_bin/l2pservices/api.svc/v1/viewAllCourseInfo?accessToken=' + token,
-		method: 'GET',
-		headers: {
-			'Content-Type': 'application/x-www-form-urlencoded',
-		}
-	};
+	var options = makeCopy();
+	options.path = '/_vti_bin/l2pservices/api.svc/v1/viewAllCourseInfo?accessToken=' + token;
+	request(options, next); 
+}
+
+function getAllDiscussions(token, cid, next) {
+	var options = makeCopy();
+	options.path = 'viewAllDiscussionItems?accessToken=' + token + '&cid=' + cid;
+	request(options, next);
+}
+
+function request(options, next) {
 	var req = https.request(options, function (res) {
 		var response = '';
 		res.setEncoding('utf8');
@@ -25,8 +52,9 @@ function getAllCourses(token, next) {
     
 	req.on('error', function (err) {
 		logger.warn(err);
-		next(err);
+		next(null, err); // TODO refactor this. swap err and null. Could have huge impact on request.
 	});
 }
 
+module.exports.getAllDiscussions = getAllDiscussions;
 module.exports.getAllCourses = getAllCourses;
