316e42059d9e75c8c3ba95156066406326f3b5b2
Johannes Neuhaus <johannes.neuhaus@rwth-aachen.de>
2015-09-10 19:50:33 +0200

    added resize for iframe

---
+++ client/js/misc/resizeme.js
@@ -0,0 +1,12 @@
+window.onresize = function(){
+    window.parent.postMessage(
+        Math.max(document.documentElement.scrollHeight, document.documentElement.offsetHeight,
+            document.body.scrollHeight, document.body.offsetHeight), "*");
+};
+
+window.onload = function() {
+    window.parent.postMessage(
+        Math.max(document.documentElement.scrollHeight, document.documentElement.offsetHeight,
+            document.body.scrollHeight, document.body.offsetHeight),"*");
+};
+
