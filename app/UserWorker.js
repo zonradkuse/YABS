7db10bb9a7c099d6a51a44c4dd6d9ff2078e75f7
Johannes Neuhaus <johannes.neuhaus@rwth-aachen.de>
2015-10-02 19:35:22 +0200

    dumb debugging oauth

--- app/UserWorker.js
+++ app/UserWorker.js
@@ -156,6 +156,7 @@ UserWorker.prototype.addRoomToSessionRights = function (roomId, accessLevel, nex
  */
 UserWorker.prototype.refreshAccessToken = function (next) {
 	var self = this;
+	logger.info("refreshing with token: " + self.user.rwth.refresh_token);
 	if (!self.user.rwth.refresh_token) {
 		return next();
 	}
