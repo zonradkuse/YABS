f765bc936fa3dc23f20b170b20de8dfa30c4888d
Johannes Neuhaus <johannes.neuhaus@rwth-aachen.de>
2015-10-25 22:13:38 +0100

    build adjusted

--- setup/release.sh
+++ setup/release.sh
@@ -2,9 +2,11 @@
 set +e
 npm install
 set -e
+cd ..
 node_modules/bower/bin/bower install
 node_modules/gulp/bin/gulp.js fast-build
 node_modules/gulp/bin/gulp.js release-build
 rm -rf public/css/
 rm public/*.js*
 rm public/*.map
+cd setup
