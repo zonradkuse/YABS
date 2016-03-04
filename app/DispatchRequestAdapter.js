6f3b6db3ee691efb5231907646419d6ce3cd14fa
Johannes Neuhaus <johannes.neuhaus@entenhausen.io>
2016-03-04 04:00:15 +0100

    fix

--- app/DispatchRequestAdapter.js
+++ app/DispatchRequestAdapter.js
@@ -1,6 +1,6 @@
 module.exports = function DispatchRequestAdapter (message, ws, rest) {
     if (ws && rest) {
-        throw new Error("Whooops, ws and app can not be set at the same time.")
+        throw new Error("Whooops, ws and app can not be set at the same time.");
     }
     this.preHook = this.postHook = noop;
 
@@ -24,19 +24,19 @@ module.exports = function DispatchRequestAdapter (message, ws, rest) {
         //                      2. /uri?identifier2=value2
         //                      3. body json text, this is most important, 2 is less important thus and can be overridden by 3.
 
-        var urlSplit = rest.req.params[0].split('/');
+        var urlSplit = rest.req.params[ 0 ].split('/');
         this.message = {
-            uri : urlSplit[0],
-            refId : urlSplit[1],
+            uri : urlSplit[ 0 ],
+            refId : urlSplit[ 1 ],
             parameters : {}
         };
 
         for (var key in rest.req.query) {
-            this.message.parameters[key] = rest.req.query[key];
+            this.message.parameters[ key ] = rest.req.query[ key ];
         }
 
     } else {
-        throw new Error("Not all cases were catched. Check request methods.")
+        throw new Error("Not all cases were catched. Check request methods.");
     }
     // remoteAdress
     // session
